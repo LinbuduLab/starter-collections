aa436d1866d99c816ce01367c6f63913f1f629be
linbudu599 <linbudu599@gmail.com>
2022-03-13 18:33:56 +0800

    feat: fix build

---
+++ packages/ts-tool-type-starter/src/tool-type.test.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tsd_1 = require("tsd");
+(0, tsd_1.expectType)(promisable);
+(0, tsd_1.expectError)(promisableError);
