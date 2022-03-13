2ecd4eb0c8dbc693a625f778d64140cceb6f292d
linbudu599 <linbudu599@gmail.com>
2022-03-13 17:41:16 +0800

    feat: react app with cra and esbuild

--- packages/vite-react-starter/src/App.tsx
+++ packages/vite-react-starter/src/App.tsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>Hello Vite + React!</p>
+        <p>Develop with LinbuduLab</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button type='button' onClick={() => setCount((count) => count + 1)}>
             count is: {count}
           </button>
         </p>
@@ -20,26 +21,26 @@ function App() {
         </p>
         <p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Learn React
           </a>
           {' | '}
           <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href='https://vitejs.dev/guide/features.html'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Vite Docs
           </a>
         </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
